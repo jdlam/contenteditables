b3660335971eb3d8af86a742fae62582eaa36d85
Jon Lam <jonlam92@gmail.com>
2016-07-14 15:27:20 -0400

    adds libs and exampl, examples, and other stuff

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
-function cleanWordPaste( in_word_text ) {
- var tmp = document.createElement("DIV");
- tmp.innerHTML = in_word_text;
- var newString = tmp.textContent||tmp.innerText;
- // this next piece converts line breaks into break tags
- // and removes the seemingly endless crap code
- newString  = newString.replace(/\n\n/g, "<br />").replace(/.*<!--.*-->/g,"");
- // this next piece removes any break tags (up to 10) at beginning
- for ( i=0; i<10; i++ ) {
-  if ( newString.substr(0,6)=="<br />" ) {
-   newString = newString.replace("<br />", "");
-  }
- }
- return newString;
-}
 // http://stackoverflow.com/questions/2875027/clean-microsoft-word-pasted-text-using-javascript
+function cleanWordPaste( in_word_text ) {
+  var tmp = document.createElement("DIV");
+  tmp.innerHTML = in_word_text;
+  var newString = tmp.textContent||tmp.innerText;
+  // this next piece converts line breaks into break tags
+  // and removes the seemingly endless crap code
+  newString  = newString.replace(/\n\n/g, "<br />").replace(/.*<!--.*-->/g,"");
+  // this next piece removes any break tags (up to 10) at beginning
+  for ( i=0; i<10; i++ ) {
+    if ( newString.substr(0,6)=="<br />" ) {
+      newString = newString.replace("<br />", "");
+    }
+  }
+  return newString;
+}
 
 console.log('loaded');
+
+$(document).ready(function() {
+  $('.asdf').on('paste', function(e){
+    e.preventDefault();
+  })
+});
